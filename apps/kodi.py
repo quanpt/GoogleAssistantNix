e56257afc2a74c5c15dc5088b8e9179c81ab1585
Quan Pham <quanpt@gmail.com>
2017-08-14 23:25:17 +1000

    Implement command validation

--- apps/kodi.py
+++ apps/kodi.py
@@ -4,13 +4,38 @@ _album = None
 _artist = None
 
 def initConversation():
+    global _album, _artist
     _album = None
     _artist = None
 
 def isValidCommand(spokenText):
+    global _album, _artist
     initConversation()
-    album = re.search(r'play album from artist (.*)', spokenText)
-    return True
+
+    artist = re.search(r'play artist (.*) please', spokenText)
+    if artist is not None:
+        _artist = artist.group(1)
+        return True
+
+    album = re.search(r'play album (.*) please', spokenText)
+    if album is not None:
+        _album = album.group(1)
+        return True
+
+    return False
 
 def executeCommand(param):
-    pass
+    print([_album, _artist])
+
+
+def main():
+    cmds = """play artist Elvis Presley please
+play artist Celine Dion please
+play album Relaxation Music Orchestra please""".split('\n')
+    for cmd in cmds:
+        print(cmd)
+        if isValidCommand(cmd):
+            executeCommand(cmd)
+
+if __name__ == '__main__':
+    main()
